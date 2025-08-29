302d09e84732ebf4fc121bdec29693da97e8ae30
Graham12-afk <maryababio39@gmail.com>
2025-08-29 18:21:49 +0000

    work

---
+++ main.py
@@ -0,0 +1,11 @@
+# Program to convert a word into uppercase
+
+# Ram writes the word in lowercase
+word = input("Enter a word: ")
+
+# Convert the word to uppercase
+uppercase_word = word.upper()
+
+# Display the congratulation message
+print("Ram is congratulating his friend:")
+print(uppercase_word, "for getting a gold medal in the state game!")
